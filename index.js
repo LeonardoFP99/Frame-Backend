3a455631a5e3a2b38abb8c9b9ff1d21a820a0afc
Leonardo Flor <leonardoflordepaula@gmail.com>
2024-02-14 21:41:41 -0300

    Environment variables import refactored

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import userRoute from './src/routes/user.route.js';
 import connectDatabase from './src/database/db.js';
-import 'dotenv/config'
+import { PORT } from './config.js';
 
 const app = express();
 
 connectDatabase();
 app.use(express.json());
 app.use('/user', userRoute);
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}...`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import 'dotenv/config'
+import { DBURL } from '../../config.js';
 
 const connectDatabase = () => {
   console.log("Trying to connect to the database...");
   mongoose
-    .connect(process.env.DBURL)
+    .connect(DBURL)
     .then(() => console.log("Successfully connected to the database"))
     .catch((error) =>
       console.log(`Error during connection to the databse: ${error.message}`)
