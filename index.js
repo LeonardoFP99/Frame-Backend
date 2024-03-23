150ccf45abd7dc2cc4e10db3f56440c5b44ee404
Leonardo Flor <leonardoflordepaula@gmail.com>
2024-03-22 21:13:28 -0300

    CORS policy

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import connectDatabase from './src/database/db.js';
 import { PORT } from './config.js';
+import cors from 'cors';
 
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
 import articleRoute from './src/routes/article.route.js';
 
 const app = express();
-
 connectDatabase();
 app.use(express.json());
+app.use(cors());
 
 app.use('/user', userRoute);
 app.use('/auth', authRoute);
